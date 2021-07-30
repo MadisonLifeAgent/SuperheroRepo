81746f4874ec83923b8b8382256d393fcfc008e5
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-30 15:08:13 -0500

    created project folder, venv, installed django, db, and superhero class

---
+++ superhero/superheroes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    primary_superpower = models.CharField(max_length=50)
+    secondary_superpower = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
