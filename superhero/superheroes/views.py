d73d02de204f203e654ed2d7c02e429295ba6f88
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-01 11:58:03 -0500

    added and revised comments. also added quote text for home page image

--- superhero/superheroes/views.py
+++ superhero/superheroes/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 
 # Create your views here.
+
 # index page function that queries db for all superheroes and returns dictionary to template
 def index(request):
     all_superheroes = Superhero.objects.all()
@@ -15,7 +16,7 @@ def index(request):
     #sends the query result(s) to urls.py for display (or to the template)
     return render(request, 'superheroes/index.html', context)
 
-# queries db for details about each superhero
+# queries db for details about a superhero
 def detail(request, superhero):
     superhero = Superhero.objects.get(id = superhero)
     context = {
@@ -39,19 +40,25 @@ def create(request):
 
         new_superhero.save()
 
+        # returns to index/home page
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
+        # displays add a new superhero form
         return render(request, 'superheroes/create.html')
 
 # deletes a super hero from superhero database
 def delete(request, superhero):
     superhero = Superhero.objects.get(id = superhero)
     
+    # delete method
     superhero.delete()
+
+    # return to index/home page
     return HttpResponseRedirect(reverse('superheroes:index'))
 
 # edits a superhero in the database
 def edit(request, superhero):
+    # get form changes or values
     if request.method == "POST":
         name = request.POST.get('name')
         alter_ego = request.POST.get('alter_ego')
@@ -59,17 +66,20 @@ def edit(request, superhero):
         secondary_superpower = request.POST.get('secondary_superpower')
         catchphrase = request.POST.get('catchphrase')
 
-        # save edits to super hero
+        # save edits to superhero
+        # make sure to add primary key id so it does not create a new row in the db
         edit_superhero = Superhero(id=superhero, name=name, alter_ego=alter_ego, primary_superpower=primary_superpower, secondary_superpower=secondary_superpower, catchphrase=catchphrase)
 
         edit_superhero.save()
 
+        # return to home page
         return HttpResponseRedirect(reverse('superheroes:index'))
 
     else:
+        # display the edit page with superhero details and form fields
         superhero = Superhero.objects.get(id = superhero)
         context = {
             'superhero': superhero
         }
-        #helps sends query results to template for display
+        
         return render(request, 'superheroes/edit.html', context)
